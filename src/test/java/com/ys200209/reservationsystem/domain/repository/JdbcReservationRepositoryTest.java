5aa5ed17d9885ec9c7f793a2e99eff276c6fae27
ys200209 <lys200209@naver.com>
2023-03-23 23:48:03 +0900

    test(reservation): 전시 정보 조회 API Repository 단위 테스트 작성

--- src/test/java/com/ys200209/reservationsystem/domain/repository/JdbcReservationRepositoryTest.java
+++ src/test/java/com/ys200209/reservationsystem/domain/repository/JdbcReservationRepositoryTest.java
@@ -1,12 +1,20 @@
 package com.ys200209.reservationsystem.domain.repository;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.ys200209.reservationsystem.domain.category.CategoriesResponseDto;
 import com.ys200209.reservationsystem.domain.category.CategoryResponseDto;
+import com.ys200209.reservationsystem.domain.display.DisplayInfoResponseDto;
+import com.ys200209.reservationsystem.domain.display.DisplayInfosRequestDto;
+import com.ys200209.reservationsystem.domain.display.DisplayInfosResponseDto;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -16,16 +24,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 public class JdbcReservationRepositoryTest {
-    private static CategoryResponseDto category1 = new CategoryResponseDto(1, "전시", 10);
-    private static CategoryResponseDto category2 = new CategoryResponseDto(2, "뮤지컬", 10);
-    private static CategoryResponseDto category3 = new CategoryResponseDto(3, "콘서트", 16);
-    private static CategoryResponseDto category4 = new CategoryResponseDto(4, "클래식", 10);
-    private static CategoryResponseDto category5 = new CategoryResponseDto(5, "연극", 13);
-
-    public static CategoriesResponseDto categories = CategoriesResponseDto.builder()
-            .size(5)
-            .items(List.of(category1, category2, category3, category4, category5))
-            .build();
+    public static CategoriesResponseDto categories = getCategoriesResponseDto();
 
     @Autowired JdbcTemplate jdbcTemplate;
 
@@ -38,13 +37,43 @@ public class JdbcReservationRepositoryTest {
 
     @Test
     void testApiCategories() {
-        // given
-
-
         // when
         CategoriesResponseDto actual = repository.getCategories();
 
         // then
         assertThat(actual).isEqualTo(categories);
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateDisplayInfosRequestDto")
+    void testApiDisplayInfos(DisplayInfosRequestDto requestDto, int expectedSize) {
+        // when
+        DisplayInfosResponseDto actual = repository.getDisplayInfos(requestDto);
+
+        // then
+//        assertThat(actual).isEqualTo(displayInfos); 모든 필드를 가지고 있지 않기 때문에 이런 간단한 방식으로는 검증할 수 없음.
+        assertThat(actual.getTotalCount()).isEqualTo(16); // totalCount 검증
+        assertThat(actual.getProductCount()).isEqualTo(4); // productCount 검증
+        assertThat(actual.getProducts().size()).isEqualTo(expectedSize); // products 개수 검증
+    }
+
+    private static CategoriesResponseDto getCategoriesResponseDto() {
+        CategoryResponseDto category1 = new CategoryResponseDto(1, "전시", 10);
+        CategoryResponseDto category2 = new CategoryResponseDto(2, "뮤지컬", 10);
+        CategoryResponseDto category3 = new CategoryResponseDto(3, "콘서트", 16);
+        CategoryResponseDto category4 = new CategoryResponseDto(4, "클래식", 10);
+        CategoryResponseDto category5 = new CategoryResponseDto(5, "연극", 13);
+
+        return CategoriesResponseDto.builder()
+                .size(5)
+                .items(List.of(category1, category2, category3, category4, category5))
+                .build();
+    }
+
+    public static Stream<Arguments> generateDisplayInfosRequestDto() {
+        return Stream.of(
+                Arguments.of(DisplayInfosRequestDto.builder().categoryId(3).start(0).build(), 4),
+                Arguments.of(DisplayInfosRequestDto.builder().categoryId(3).start(14).build(), 2)
+        );
     }
 }
