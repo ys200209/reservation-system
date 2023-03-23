18d5815deb25a7d01a3a1bd6d6b6dcb822a9783b
ys200209 <lys200209@naver.com>
2023-03-23 23:47:52 +0900

    test(reservation): 전시 정보 조회 API Service 단위 테스트 작성

--- src/test/java/com/ys200209/reservationsystem/domain/service/ReservationServiceTest.java
+++ src/test/java/com/ys200209/reservationsystem/domain/service/ReservationServiceTest.java
@@ -1,21 +1,23 @@
 package com.ys200209.reservationsystem.domain.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import com.ys200209.reservationsystem.domain.category.CategoriesResponseDto;
-import com.ys200209.reservationsystem.domain.repository.JdbcReservationRepository;
+import com.ys200209.reservationsystem.domain.display.DisplayInfoResponseDto;
+import com.ys200209.reservationsystem.domain.display.DisplayInfosRequestDto;
+import com.ys200209.reservationsystem.domain.display.DisplayInfosResponseDto;
 import com.ys200209.reservationsystem.domain.repository.JdbcReservationRepositoryTest;
 import com.ys200209.reservationsystem.domain.repository.ReservationRepository;
-import org.junit.jupiter.api.AfterEach;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-class ReservationServiceTest {
+public class ReservationServiceTest {
     private CategoriesResponseDto categories = JdbcReservationRepositoryTest.categories;
 
     @Mock
@@ -38,5 +40,39 @@ class ReservationServiceTest {
 
         // then
         assertThat(actual).isEqualTo(categories);
+    }
+
+    @Test
+    void testApiDisplayInfos() {
+        // given
+        DisplayInfosRequestDto requestDto = DisplayInfosRequestDto.builder()
+                .categoryId(3)
+                .start(0)
+                .build();
+
+        // when
+        when(repository.getDisplayInfos(any(DisplayInfosRequestDto.class))).thenReturn(generateDisplayInfosResponseDto());
+
+        DisplayInfosResponseDto actual = service.getDisplayInfos(requestDto);
+
+        // then
+        assertThat(actual.getTotalCount()).isEqualTo(16); // totalCount 검증
+        assertThat(actual.getProductCount()).isEqualTo(4); // productCount 검증
+        assertThat(actual.getProducts().size()).isEqualTo(4); // products 개수 검증
+    }
+
+    public static DisplayInfosResponseDto generateDisplayInfosResponseDto() {
+        return DisplayInfosResponseDto.builder()
+                .totalCount(16)
+                .productCount(4)
+                .products(
+                        List.of(
+                                DisplayInfoResponseDto.builder().build(),
+                                DisplayInfoResponseDto.builder().build(),
+                                DisplayInfoResponseDto.builder().build(),
+                                DisplayInfoResponseDto.builder().build()
+                        )
+                )
+                .build();
     }
 }
