7e01790d94c86276bf2db59681fe9c9b9a0c4775
ys200209 <lys200209@naver.com>
2023-03-23 23:47:43 +0900

    test(reservation): 전시 정보 조회 API Controller 단위 테스트 작성

--- src/test/java/com/ys200209/reservationsystem/web/controller/ReservationControllerTest.java
+++ src/test/java/com/ys200209/reservationsystem/web/controller/ReservationControllerTest.java
@@ -1,65 +1,41 @@
 package com.ys200209.reservationsystem.web.controller;
 
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ys200209.reservationsystem.domain.category.CategoriesResponseDto;
 import com.ys200209.reservationsystem.domain.display.DisplayInfosRequestDto;
-import com.ys200209.reservationsystem.domain.display.DisplayInfosResponseDto;
 import com.ys200209.reservationsystem.domain.repository.JdbcReservationRepositoryTest;
 import com.ys200209.reservationsystem.domain.service.ReservationService;
-import com.ys200209.reservationsystem.utils.restdocs.RestDocsFieldsGenerator;
-import com.ys200209.reservationsystem.utils.restdocs.RestDocsGenerator;
-import com.ys200209.reservationsystem.utils.restdocs.RestDocsTemplate;
+import com.ys200209.reservationsystem.domain.service.ReservationServiceTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
-import org.springframework.restdocs.templates.TemplateFormats;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-@WebMvcTest(ReservationController.class)
-@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 class ReservationControllerTest {
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    @MockBean
-    ReservationService service;
+    @InjectMocks
+    private ReservationController controller;
+
+    @Mock
+    private ReservationService service;
 
     private MockMvc mockMvc;
 
-    @Autowired
-    private WebApplicationContext context;
-
     @BeforeEach
-    void setUp(RestDocumentationContextProvider restDocumentation) {
+    void setUp() {
         MockitoAnnotations.openMocks(this);
-        mockMvc = MockMvcBuilders.webAppContextSetup(context)
-                .apply(documentationConfiguration(restDocumentation).snippets()
-                                .withTemplateFormat(TemplateFormats.asciidoctor())
-                        /*.withTemplateFormat(TemplateFormats.markdown())*/)
-                .build();
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
@@ -77,23 +53,28 @@ class ReservationControllerTest {
                 .andExpect(jsonPath("$.items[0].count").value(10))
                 .andExpect(jsonPath("$.items[4].id").value(5))
                 .andExpect(jsonPath("$.items[4].name").value("연극"))
-                .andExpect(jsonPath("$.items[4].count").value(13))
-                .andDo(RestDocsGenerator.generate("/api/categories", null, new CategoriesResponseDto()));
+                .andExpect(jsonPath("$.items[4].count").value(13));
     }
 
     @Test
     void testApiDisplayInfos() throws Exception {
         // given
         DisplayInfosRequestDto requestDto = DisplayInfosRequestDto.builder()
-                .categoryId(0)
+                .categoryId(3)
                 .start(0)
                 .build();
+
+        // when
+        when(service.getDisplayInfos(any(DisplayInfosRequestDto.class))).thenReturn(ReservationServiceTest.generateDisplayInfosResponseDto());
 
         // then
         mockMvc.perform(get("/api/displayinfos")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(requestDto)))
                 .andExpect(status().isOk())
-                .andDo(RestDocsGenerator.generate("/api/displayinfos", new DisplayInfosRequestDto(), new DisplayInfosResponseDto()));
+                .andExpect(jsonPath("$.totalCount").value(16))
+                .andExpect(jsonPath("$.productCount").value(4))
+                .andExpect(jsonPath("$.products").isArray())
+                .andExpect(jsonPath("$.products", hasSize(4)));
     }
 }
