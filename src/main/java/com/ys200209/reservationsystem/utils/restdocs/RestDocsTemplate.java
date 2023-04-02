5a7ed6dcc05b7b3535d9f83fb2898145f47dea89
ys200209 <lys200209@naver.com>
2023-04-02 17:15:48 +0900

    feat(reservation): 경로 변수 API 문서화 기능 추가

--- src/main/java/com/ys200209/reservationsystem/utils/restdocs/RestDocsTemplate.java
+++ src/main/java/com/ys200209/reservationsystem/utils/restdocs/RestDocsTemplate.java
@@ -1,7 +1,11 @@
 package com.ys200209.reservationsystem.utils.restdocs;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.List;
 
 public interface RestDocsTemplate {
     List<RestDocsDto> generateRestDocsFields(String rootField);
+
+    @JsonIgnore
+    boolean isPathParameters();
 }
